c7724969f812b4fbf8c63966fd640f8c74a87cd8
soybeins <renzolebumfacil@gmail.com>
2021-06-08 12:38:32 +0800

    added crud for scouts and players

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -4,7 +4,7 @@ const player = require("../models/player");
 exports.display = (req,res) =>{
     if(req.session.uuid){
         player.model.findAll().then(player =>{
-            res.render("players", {player:player,username:req.session.username});
+            res.render("players", {player:player,username:req.session.username,uuid: req.session.uuid});
         }).catch(err => {
             console.log(err);
         })
@@ -13,10 +13,16 @@ exports.display = (req,res) =>{
     }
 }
 
-//Render 
-exports.scout_edit = (req,res) =>{
+//Render edit player page
+exports.player_edit = (req,res) =>{
     if(req.session.uuid){
-        res.render('update-player',{username:req.session.username});
+        player.model.findOne({
+                where:
+                {
+                    id:req.query.id
+                }}).then(result => {
+                    res.render('update-player',{username:req.session.username,player:result});
+                })
     }else{
         res.redirect("/");
     }
@@ -58,11 +64,47 @@ exports.display_position = (req,res) =>{
                 position:req.query.pos
             }})
         .then(player =>{
-            res.render("players", {player:player,username:req.session.username});
+            res.render("players", {player:player,username:req.session.username,uuid: req.session.uuid});
         }).catch(err => {
             console.log(err);
         })
     }else{
         res.redirect("/");
     }
+}
+
+exports.player_update = async(req,res) => {
+    await player.model.update({
+        photo: req.body.photo,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        position: req.body.position,
+        height: req.body.height,
+        weight: req.body.weight,
+        age: req.body.age,
+        rating: req.body.rating,
+        address: req.body.address 
+    },{
+        where: { id: req.body.id }
+    }).then(result =>{
+        if(result){
+            console.log("Succesfully updated player!");
+            res.redirect("/player");
+        }
+    }).catch(err => {
+        console.log(err);
+        res.redirect("/player/player_edit");
+    })
+}
+
+exports.player_delete = async(req, res) => {
+    await player.model.destroy({
+        where: { 
+            id: req.query.id
+        }}).then(result => {
+            if(result){
+                console.log("Succesfully deleted");
+                res.redirect("/player");
+            }
+        })
 }
--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -5,7 +5,10 @@ const playerController = require("../controllers/playerController");
 router.get("/", playerController.display);
 router.get("/create", playerController.create);
 router.get("/display_position", playerController.display_position);
+router.get("/player_edit", playerController.player_edit);
+router.get("/player_delete", playerController.player_delete);
 
 router.post("/create_player", playerController.create_player);
+router.post("/player_update", playerController.player_update);
 
 module.exports = router;
